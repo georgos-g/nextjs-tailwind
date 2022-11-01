2ff8fcb0a92ef9960c921043ad3ab77cd85abd5c
georgos-g <gg@dancingbear.de>
2022-11-01 10:56:45 +0100

    changed fetching project/index.js

--- pages/projects/index.js
+++ pages/projects/index.js
@@ -30,57 +30,57 @@ export default function Projects({ posts, allCategories }) {
               >
                 ALL
               </span>
-              {/* map thru allCategories and place the results in a list    */}
-              {allCategories.map((navCategory, index) => {
-                return (
-                  <>
-                    <span
-                      key={index}
-                      className='px-1 py-1 text-sm text-gray-800 uppercase hover:text-gray-400 md:text-base md:px-3'
-                      data-filter={`.${navCategory.category}`}
-                    >
-                      {navCategory.category}
-                    </span>
-                  </>
-                );
-              })}
+              {/* check if allCategories exist, map thru them and place the results in a list    */}
+              {allCategories &&
+                allCategories.map((navCategory, index) => (
+                  <span
+                    key={index}
+                    className='px-1 py-1 text-sm text-gray-800 md:text-base md:px-3 hover:text-gray-400'
+                    data-filter={`.${navCategory.category}`}
+                  >
+                    {navCategory.category.toUpperCase()}
+                  </span>
+                ))}
             </div>
           </div>
         </div>
 
         {/*  ========== Posts Content Wrapper =========*/}
         <div className='max-w-[1920px] pb-2 pt-4 flex flex-col md:flex-row flex-wrap justify-center w-full mx-auto gallery'>
-          {/* map thru posts and save the results in a list sorted by date */}
-          {posts.map((post, index) => (
-            <div className='' key={index}>
-              <Link href={`projects/${post.slug}`} key={post.id}>
-                {/* ======= Item ======== */}
-                <div
-                  // set 'items' and category in css class to activate filtering
-                  className={`lg:w-1/3 md:w-1/2  pb-1 md:pb-0 items ${post.categories
-                    .toString()
-                    // replace comma with space
-                    .replace(/,/g, ' ')} `}
-                >
-                  {/* Img and Text */}
-                  <div className='flex flex-col justify-center w-full h-full z-1'>
-                    <Image
-                      className='object-cover'
-                      width={999}
-                      height={444}
-                      src={post.cover}
-                      alt={post.alt}
-                    />
-                    {/* Hover  */}
+          {/* check if posts exist map thru them and save the results in a list sorted by date */}
+          {posts &&
+            posts
+              .sort((a, b) => new Date(b.date) - new Date(a.date))
+              .map((post, index) => (
+                <div className='' key={index}>
+                  <Link href={`projects/${post.slug}`} key={post.id}>
+                    {/* ======= Item ======== */}
+                    <div
+                      // set 'items' and category in css class to activate filtering
+                      className={`lg:w-1/3 md:w-1/2  pb-1 md:pb-0 items ${post.categories
+                        .toString()
+                        // replace comma with space
+                        .replace(/,/g, ' ')} `}
+                    >
+                      {/* Img and Text */}
+                      <div className='flex flex-col justify-center w-full h-full z-1'>
+                        <Image
+                          className='object-cover'
+                          width={999}
+                          height={444}
+                          src={post.cover}
+                          alt={post.alt}
+                        />
+                        {/* Hover  */}
 
-                    <div className='absolute z-10 w-full h-full opacity-100 cursor-pointer md:opacity-0 bg-black/70 md:hover:opacity-100 hover:transition hover:ease-in-out hover:duration-1000'>
-                      {/* Hover Content */}
+                        <div className='absolute z-10 w-full h-full opacity-100 cursor-pointer md:opacity-0 bg-black/70 md:hover:opacity-100 hover:transition hover:ease-in-out hover:duration-1000'>
+                          {/* Hover Content */}
 
-                      {/* tailwindcss move div on Hover 20px to right  */}
-                      <div className='flex flex-col justify-center w-full h-full px-8 hover:duration-1000 hover:translate-x-4'>
-                        {/* Category */}
+                          {/* tailwindcss move div on Hover 20px to right  */}
+                          <div className='flex flex-col justify-center w-full h-full px-8 hover:duration-1000 hover:translate-x-4'>
+                            {/* Category */}
 
-                        {/* <span
+                            {/* <span
                           data-filter={post.categories
                             .toString()
                             .replace(/,/g, ' ')}
@@ -88,21 +88,21 @@ export default function Projects({ posts, allCategories }) {
                           {post.categories.toString().replace(/,/g, ' ')}
                         </span> */}
 
-                        {/* Title */}
-                        <h1 className='mb-2 text-[1em] xs:text-[1.25em] text-white uppercase'>
-                          {post.title}
-                        </h1>
-                        {/* Description */}
-                        <p className='text-gray-400 text-[0.8em] xs:text-[1em] pb-6 uppercase'>
-                          {post.description}
-                        </p>
+                            {/* Title */}
+                            <h1 className='mb-2 text-[1em] xs:text-[1.25em] text-white uppercase'>
+                              {post.title}
+                            </h1>
+                            {/* Description */}
+                            <p className='text-gray-400 text-[0.8em] xs:text-[1em] pb-6 uppercase'>
+                              {post.description}
+                            </p>
+                          </div>
+                        </div>
                       </div>
                     </div>
-                  </div>
+                  </Link>
                 </div>
-              </Link>
-            </div>
-          ))}
+              ))}
         </div>
       </section>
     </>
