c04d34d9b05831ec9e7cee3008fa2e06c4a420ba
Georgos Gakis <georgos_gakis@intention.de>
2023-05-29 12:17:57 +0200

    removed fontawesome-svg-core

--- pages/_app.js
+++ pages/_app.js
@@ -6,10 +6,10 @@ import ScrollToTop from '../components/ScrollToTop';
 import App from 'next/app';
 
 // import Font Awesome CSS
-import '@fortawesome/fontawesome-svg-core/styles.css';
-import { config } from '@fortawesome/fontawesome-svg-core';
+// import '@fortawesome/fontawesome-svg-core/styles.css';
+// import { config } from '@fortawesome/fontawesome-svg-core';
 // Tell Font Awesome to skip adding the CSS automatically since it's being imported above
-config.autoAddCss = false;
+// config.autoAddCss = false;
 
 function MyApp({ Component, pageProps }) {
   return (
