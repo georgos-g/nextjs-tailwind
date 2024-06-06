426cc42f7019509b45e8ebde1a61f66e67d1eb2e
georgos-g <65546167+georgos-g@users.noreply.github.com>
2024-06-06 23:48:51 +0200

    chore: Refactor BgImgFull component for improved styling and responsiveness

--- pages/style_test.js
+++ pages/style_test.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import BgImgFull from '../components/BgImgFull';
+import Navbar from '../components/Navbar';
 
 export default function StyleTest() {
   return (
-    <div className='relative_ '>
+    <div className='w-full sm:h-full bg-gradient-to-tl from-yellow-800 to-gray-900'>
+      <Navbar />
       <BgImgFull background='/img/hero_bg.jpg' as='image'>
         <div className='absolute inset-0 flex items-center justify-center mb-20 xl:pl-10'>
           <div className=' text-center relative flex flex-col h-[160px] max-w-[750px] px-14 py-6 items-center justify-center border-2 border-yellow-500 mb-28 bg-black bg-opacity-60 text-white'>
